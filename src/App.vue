334175e092c1a60144115b563c027b7ddec6cff3
longsi <zhulixing@xiaohongshu.com>
2023-04-21 13:55:26 +0800

    feat: uni test init

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script>
+import { onReady, onLaunch, onShow, onHide } from '@dcloudio/uni-app';
+import { defineComponent } from 'vue';
+
+// export default {
+//   onLaunch: function () {
+//     console.log('App Launch')
+//   },
+//   onShow: function () {
+//     console.log('App Show')
+//   },
+//   onHide: function () {
+//     console.log('App Hide')
+//   },
+// }
+export default defineComponent({
+  setup() {
+    onLaunch(() => {
+      console.log('App Launch')
+    })
+    onShow(() => {
+      console.log('App Show')
+    })
+    onHide(() => {
+      console.log('App Hide')
+    })
+  }
+})
+</script>
+
+<style>
+/*每个页面公共css */
+</style>
